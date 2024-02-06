5c8216ce95289777ca6e67a077139a4c2e9c029a
matsuhisa <matsuhisa+github@gmail.com>
2024-02-07 00:18:52 +0900

    Move ./components/ui to ./components/ui/Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Button } from './components/ui/Button'
+import { Button } from './components/ui/button/Button'
 import './App.css'
 
 function App() {
